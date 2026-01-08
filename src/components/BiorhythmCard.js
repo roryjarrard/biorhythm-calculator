f81cace4bf83b0199767a751223ca63861e14bad
Rory Jarrard <roryjarrard@gmail.com>
2026-01-08 10:51:48 -0700

    add chart styles

--- src/components/BiorhythmCard.js
+++ src/components/BiorhythmCard.js
@@ -2,6 +2,7 @@ import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from "@ionic/rea
 import dayjs from "dayjs"
 import { calculateBiorhythms } from "../lib/biorhythms"
 import BiorhythmChart from "./BiorhythmChart"
+import './BiorhythmCard.css'
 
 function formatDate(isoString) {
     const day = dayjs(isoString)
@@ -11,15 +12,15 @@ function formatDate(isoString) {
 function BiorhythmCard({ dob, targetDate }) {
     const biorhythms = calculateBiorhythms(dob, targetDate)
     return (
-        <IonCard className="ion-text-center">
+        <IonCard className="ion-text-center BiorhythmCard">
             <IonCardHeader>
                 <IonCardTitle>{formatDate(targetDate)}</IonCardTitle>
             </IonCardHeader>
             <IonCardContent>
                 <BiorhythmChart birthDate={dob} targetDate={targetDate} />
-                <p>Physical: {biorhythms.physical.toFixed(4)}</p>
-                <p>Emotional: {biorhythms.emotional.toFixed(4)}</p>
-                <p>Intellectual: {biorhythms.intellectual.toFixed(4)}</p>
+                <p className="physical">Physical: {biorhythms.physical.toFixed(4)}</p>
+                <p className="emotional">Emotional: {biorhythms.emotional.toFixed(4)}</p>
+                <p className="intellectual">Intellectual: {biorhythms.intellectual.toFixed(4)}</p>
             </IonCardContent>
         </IonCard>
     )
